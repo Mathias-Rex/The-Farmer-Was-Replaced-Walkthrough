8273022e352f16adafc9503954a21ef458f5ecf4
exphoenee <bozzay.viktor@gmail.com>
2026-04-23 16:02:15 +0200

    harvest() sokszor

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-# Ez az első parancs amit tudunk használni. Ezzel learatjuk azt a területet, ami felett a drónunk éppen áll.
-# Ha a növény még nem fejlődött ki akkor kárba vész a termény, ezért előfordulhat, hogy várnuink kell. Ezt megtehetjük a do_a_flip() függvény meghívásával, ami 1 másodpercig tart.
 # A célunk most, az hogy a while függvényt kiniythassuk.
+# Ezért, hogy ne kellejen a play gombot nyopmogatni, írjuk be többször egymás utána harvest függvényt.
 
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
+harvest()
 harvest()
