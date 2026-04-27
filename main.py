3fc962aed6e720ddc44911a1508ad39e852733cd
exphoenee <bozzay.viktor@gmail.com>
2026-04-27 18:55:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# Most már tényleg oda tudjuk küldeni,
-# ahová csak akarjuk.
+# Ne kapáljunk ha nem kell.
+# Második lépésként pedig ne mozogjunk feleslegesen.
+# Aktiváljuk a kommentelt a sorokat a kódban, és a
+# goto(x, y)-t pedig töröljük, vagy kommenteljük ki.
 
 def goto(x, y):
     while x != get_pos_x() or y != get_pos_y():
@@ -15,11 +17,16 @@ def goto(x, y):
 while True:
     for y in range(3):
         for x in range(3):
+            #rx = x
+            #if y % 2 != 0:
+            #   rx = 3 - x - 1
+            #goto(rx, y)
             goto(x, y)
             if can_harvest():
                 harvest()
             if get_pos_y() == 0:
-                till()
+                if get_ground_type() == Grounds.Grassland:
+                    till()
                 plant(Entities.Carrot)
             if get_pos_y() == 1:
                 plant(Entities.Bush)
