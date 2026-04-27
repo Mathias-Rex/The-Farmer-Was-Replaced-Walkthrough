cde4118784732ac969177233c6ede61fdb46736e
exphoenee <bozzay.viktor@gmail.com>
2026-04-27 19:20:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-harvest()
+# Késítsünk egy okos talaj előkészítő függvényt.
+
+def goto(x, y):
+    while x != get_pos_x() or y != get_pos_y():
+        if x > get_pos_x():
+            move(East)
+        elif x < get_pos_x():
+            move(West)
+        if y > get_pos_y():
+            move(North)
+        elif y < get_pos_y():
+            move(South)
+
+def prepare_ground(req_ground):
+    if get_ground_type() != req_ground:
+        till()
+
+def plant_carrot():
+    if num_items(Items.Wood) > 1 or num_items(Items.Hay) > 1:
+        prepare_ground(Grounds.Soil)
+        plant(Entities.Carrot)
+    else:
+        plant_bush() # itt is cseréljük ki, így nem fogja talajra ültetni a bokrot
+
+def plant_grass():
+    prepare_ground(Grounds.Grassland)
+
+def plant_bush():
+    prepare_ground(Grounds.Grassland)
+    plant(Entities.Bush)
+
+while True:
+    for y in range(get_world_size()):
+        for x in range(get_world_size()):
+            rx = x
+            if y % 2 != 0:
+                rx = get_world_size() - x - 1
+            goto(rx, y)
+            if can_harvest():
+                harvest()
+            if get_pos_y() == 0 or get_pos_y() == 2:
+                plant_carrot()
+            if get_pos_y() == 1:
+                plant_bush()
