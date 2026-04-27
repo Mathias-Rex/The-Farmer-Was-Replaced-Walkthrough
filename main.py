1459ecac08704bb606f680de75ac1f981af6fb92
exphoenee <bozzay.viktor@gmail.com>
2026-04-27 17:46:22 +0200

    for x and y

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# Nyissuk ki a most a Bush-t is.
-# Cseréljünk kalapot és
-# ültessünk és arassunk bokrot.
-# Bokor sokkal lasabbaan nő, mint a fű, 
-# ezért rendezzük át a kódot kicsit:
-# ültetlés után hagyjuk nőni a bokrot és
-# menjünk egyet északra.
-# Még így is előfordul néha, hogy a
-# drónunk túl gyors...    
+# Gyűjtsünk 20 fát, majd az Expandot újra.
+# 3x3-as mező a jutalmunk + a for loop.
+# A for loopot tudjuk hasonlítani a Scratch
+# ismételd blokkjához, cset iss azt is megkapjuk,
+# hogy éppen hányadik ismétlésnél tartunk!
+# Fontos megértetni a gyerekekkel, hogy 0-ról
+# indul a számolás!
+# Kaptunk egy clear függvényt is, amit ha
+# meghívunk a programunk elején "tiszta lappal"
+# indulunk!
+# Először while loop nélkül mutassuk mag a
+# gyerekeknek, majd az egészet tegyük while-ba.
+# Itt ki is térhetünk rá, hogy a teli nyíl futtatja
+# a programot, az üres nyil pedig csak egy sort futtat,
+# aztán megáll.
 
-change_hat(Hats.Green_Hat)
-while True:
-    plant(Entities.Bush)
+clear()
+for y in range(3):
+    for x in range(3):
+        plant(Entities.Bush)
+        move(East)
+        if can_harvest():
+            harvest()
     move(North)
-    if can_harvest():
-        harvest()
+    
+# Töröld a felső kódot, hogy ez futhasson.
+    
+clear()
+while True:
+for y in range(3):
+    for x in range(3):
+        plant(Entities.Bush)
+        move(East)
+        if can_harvest():
+            harvest()
+    move(North)
