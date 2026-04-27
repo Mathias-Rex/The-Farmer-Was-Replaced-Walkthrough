d130cd255fb3e6b3a0c1cf939242afe6418ff851
exphoenee <bozzay.viktor@gmail.com>
2026-04-27 16:42:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # A célunk most, az hogy a while függvényt kiniythassuk.
-# Ezért, hogy ne kellejen a play gombot nyopmogatni, írjuk be többször egymás utána harvest függvényt.
+# Ezért, hogy ne kellejen a play gombot nyopmogatni, írjuk
+# be többször egymás utána harvest függvényt.
 
 harvest()
 harvest()
