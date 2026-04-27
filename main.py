600ce7b8d978ab56e9da0011734545138015b1a5
exphoenee <bozzay.viktor@gmail.com>
2026-04-27 20:21:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-# Csakhogy így nagyon gyorsan elfogy a vizünk
-# és sárga figyelmeztéts kapunk mint a répánál.
-# Építsünk be védelmet, mint a répa esetében!
-# Tehát a utiltiy-ben a prepare_ground függvényt
-# módosítjuk megint!
+# Nyissuk ki a Water-t megint, hogy többet tudjunk
+# locsolni. Ahhoz hogy még többet tudjunk locsolni
+# harmadszor is ki kellene nyitnunk, de ahhoz
+# 800 fa kell!
+# Okosítsuk a rendszert! Készítsünk limiteket és
+# döntsön a drón, mit termelünk!
 
 import field
 
+req_wood = 850
+req_carrot = 500
+req_hay = 800
+
 while True:
-    field.carrot()
-    field.forest()
-    field.grass()
+    if num_items(Items.Hay) < req_hay:
+        field.grass()
+    elif num_items(Items.Wood) < req_wood:
+        field.forest()
+    else:
+        field.carrot()
