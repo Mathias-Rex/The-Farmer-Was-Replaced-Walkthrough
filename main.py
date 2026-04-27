92deda776583e41ed89bdf3595ef214c96cf7c38
exphoenee <bozzay.viktor@gmail.com>
2026-04-27 18:20:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-# Termeljünk mindent!
-# mivel (majdnem) tudjuk, hogy hányadik sorban
-# vagy oszlopban vagyunk, ezért
-# megmondhatjuk, hogy mit kell csinálnunk!
+# Nyissuk ki az Operators és a Senses is.
+# Máris tudjuk, hogy a drónunk.
 
 while True:
     for y in range(3):
         for x in range(3):
             if can_harvest():
                 harvest()
-            if y == 0:
+            if get_pos_y() == 0:
                 till()
                 plant(Entities.Carrot)
-            if y == 1:
+            if get_pos_y() == 1:
                 plant(Entities.Bush)
             move(East)
         move(North)
-
-# Miért van az, hogy hol az első sorba,
-# hol a másodikba kerül a répa?
-# - Ez attól függ, hogy melyik sorban áll
-#   a frónunk a program indításakor.
-#   valójában a drón nem tudja, hol van,
-#   csak azt modntuk neki, hogy 3x készíts egy sort
-#   (azaz 3x arass, ültess, menj jobbra jobbra),
-#   aztán menjd felfelé
