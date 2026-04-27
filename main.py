60625e294c45005916a5c5864a2963179958542f
exphoenee <bozzay.viktor@gmail.com>
2026-04-27 16:43:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# Most nyissuk ki a Speed-et (a while alatt), hogy gyorsabban termeljük a szalmát.
-# Most láthatjuk, hogy a dónunk már gyorsabb, mint
+# Most nyissuk ki a Speed-et (a while alatt), hogy gyorsabban termeljük a
+# szalmát. Most láthatjuk, hogy a dónunk már gyorsabb, mint
 # ahogyan a fű kinő, úgyhogy nem termelünk semmit.
 # Ezért a do_a_flip() függvényt használjuk, hogy megszerzzük a szalmát.
 
