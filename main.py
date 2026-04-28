e3ab9ed4d3b9cf805ccadaf363c78d12720b9e32
exphoenee <bozzay.viktor@gmail.com>
2026-04-28 10:48:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ while True:
             if y % 2 != 0:
                 rx = get_world_size() - x - 1
             utility.goto(rx, y)
-            utility.harvest_if_possible()
             plant(custom_map[y][rx])
 
 #    if num_items(Items.Hay) < req_hay:
